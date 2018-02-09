62bff0752d018264110a047584d083220f0bc001
kostiantyn13 <kos.kondratenko@gmail.com>
2018-02-09 17:26:11 +0200

    8

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-//import App from "./App";
+import App from "./App";
 
-function App() {
-  return <div />;
-}
 ReactDOM.render(<App />, document.getElementById("root"));
