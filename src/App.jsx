8bedfea5f34602f95d488780a6e814fdb5710dd4
kostiantyn13 <kos.kondratenko@gmail.com>
2018-02-09 15:54:17 +0200

    8

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import Header from "./components/header.jsx";
-import BoxMenu from "./components/boxMenu.jsx";
+import BoxMenu from "./components/box-menu.jsx";
 import Field from "./components/field.jsx";
 import Footer from "./components/footer.jsx";
 import HeaderMenu from "./components/header-menu.jsx";
 import styled from "styled-components";
-
+/*
 class App extends Component {
   render() {
     return (
@@ -24,4 +24,25 @@ class App extends Component {
 export default App;
 const Sty = styled.div`
   min-width: 800px;
-`;
+  margin: 0;
+  padding: 0;
+`;*/
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>React TODO</h1>
+      </header>
+      <section className="todo-list">
+        <div className="todo">
+          <button className="checbox icon">
+            <i className="material-icons">check_box_outline_blank</i>
+          </button>
+
+          <span className="todo-title">Изучить REACT</span>
+        </div>
+      </section>
+    </div>
+  );
+}
+export default App;
