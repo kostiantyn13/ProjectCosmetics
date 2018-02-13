55939d65de20b93495cc46f579c3e4225173ef0b
Kostiantyn <kos.kondratenko@gmail.com>
2018-02-13 20:46:42 +0200

    10

---
+++ src/components/fast.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+
+class Goods extends Component {
+  render() {
+    return (
+      <Sty>
+        <div>Goods</div>
+      </Sty>
+    );
+  }
+}
+
+export default Goods;
+const Sty = styled.div`
+  height: 150px;
+  width: 100%;
+  border: 1px solid black;
+  border-radius: 5px;
+  background-color: #e2dced;
+`;
