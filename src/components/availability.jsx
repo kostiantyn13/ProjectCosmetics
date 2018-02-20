77d99121cb40db616aeabed668be6217f38fc75c
Kostiantyn <kos.kondratenko@gmail.com>
2018-02-21 01:04:50 +0200

    10

--- src/components/availability.jsx
+++ src/components/availability.jsx
@@ -1,15 +1,16 @@
 import React, { Component } from "react";
+import Button from "material-ui/Button";
 
 class Availability extends Component {
   render() {
     const { available } = this.props;
     //const available = this.props.available
     return (
-      <button>
+      <Button>
         {available === 1 && "В наличии"}
         {available === 2 && "Нет в наличии"}
         {available === 3 && "Под заказ"}
-      </button>
+      </Button>
     );
   }
 }
