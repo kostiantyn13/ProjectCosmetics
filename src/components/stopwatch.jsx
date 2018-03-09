aba29d7b639f31c1d2b239807bd4f891f399e139
Kostiantyn <kos.kondratenko@gmail.com>
2018-03-09 16:57:53 +0200

    upgrade stopwatch

--- src/components/stopwatch.jsx
+++ src/components/stopwatch.jsx
@@ -1,12 +1,38 @@
 import React, { Component } from "react";
 import styled from "styled-components";
-import Timer from "./timer";
+import Button from "material-ui/Button";
 
 class Stopwatch extends Component {
+  constructor(props) {
+    super(props);
+    this.buttonTimer = this.buttonTimer.bind(this);
+    this.state = {
+      timer: true,
+      timerHour: "00",
+      timerMinute: "00",
+      timerSecond: "00"
+    };
+  }
+
+  buttonTimer() {
+    this.setState({
+      timer: this.state.timer ? false : true,
+      timerHour: "22",
+      timerMinute: "33",
+      timerSecond: "44"
+    });
+  }
   render() {
+    const { state } = this;
     return (
       <Watch>
-        <Timer />
+        <Scoreboard>
+          {state.timerHour}:{state.timerMinute}:{state.timerSecond}
+        </Scoreboard>
+
+        <Button onClick={this.buttonTimer}>
+          {state.timer ? "Стоп" : "Старт"}
+        </Button>
       </Watch>
     );
   }
@@ -15,7 +41,13 @@ class Stopwatch extends Component {
 export default Stopwatch;
 
 const Watch = styled.div`
-  height: 50px;
-  width: 200px;
+  height: 36px;
+  width: 220px;
   border: 1px solid #669900;
+`;
+const Scoreboard = styled.div`
+  display: block;
+  float: left;
+  padding: 10px;
+  width: 100px;
 `;
