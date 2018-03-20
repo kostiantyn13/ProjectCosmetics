44d92598271473d84cec1f9d8c8cf5173efce41c
kostiantyn13 <kos.kondratenko@gmail.com>
2018-03-20 16:59:10 +0200

    counter

--- src/counter.jsx
+++ src/counter.jsx
@@ -1,4 +1,25 @@
 import React, { Component } from "react";
+import Store from "./reducer";
+
+const initialState = { count: 0 };
+const incrementAction = { type: "INCREMENT", amount: 1 };
+const decrementAction = { type: "DECREMENT", amount: 1 };
+const resetAction = { type: "RESET" };
+
+function updateState(state, action) {
+  switch (action.type) {
+    case "INCREMENT":
+      return { count: state.count + action.amount };
+    case "DECREMENT":
+      return { count: state.count - action.amount };
+    case "RESET":
+      return { count: 0 };
+    default:
+      return state;
+  }
+}
+
+const store = new Store(updateState, initialState);
 
 class Counter extends Component {
   constructor(props) {
@@ -10,23 +31,33 @@ class Counter extends Component {
 
     this.increment = this.increment.bind(this);
     this.decrement = this.decrement.bind(this);
+    this.reset = this.reset.bind(this);
+  }
+
+  componentDidMount() {
+    store.subscribe(() => this.forceUpdate());
   }
 
   increment() {
-    this.setState({ count: this.state.count + 1 });
+    store.updete(incrementAction);
   }
 
   decrement() {
-    this.setState({ count: this.state.count - 1 });
+    store.updete(decrementAction);
+  }
+
+  reset() {
+    store.updete(resetAction);
   }
 
   render() {
     return (
       <div>
-        <span>{this.state.count}</span>
+        <span>{store.state.count}</span>
         <div>
           <button onClick={this.decrement}>-</button>
           <button onClick={this.increment}>+</button>
+          <button onClick={this.reset}>Reset</button>
         </div>
       </div>
     );
