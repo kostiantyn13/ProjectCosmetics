89264be48d36ff1abecf330f4330140b8c6a73b3
kostiantyn13 <kos.kondratenko@gmail.com>
2018-03-20 13:46:22 +0200

    added subscribe

--- reducer.js
+++ reducer.js
@@ -1,10 +1,8 @@
-let state = 0;
-
 function updateState(state, action) {
   if (action.type === "INCREMENT") {
-    return state + action.amount;
+    return { count: state.count + action.amount }; //c уя ли это именно свойство обьекта state??
   } else if (action.type === "DECREMENT") {
-    return state - action.amount;
+    return { count: state.count - action.amount };
   } else {
     return state;
   }
@@ -14,6 +12,7 @@ class Store {
   constructor(updateState, state) {
     this._state = state;
     this._updateState = updateState;
+    this._callback = [];
   }
 
   get state() {
@@ -22,10 +21,30 @@ class Store {
 
   updete(action) {
     this._state = this._updateState(this._state, action);
+    this._callback.forEach(callback => callback());
+  }
+
+  subscribe(callback) {
+    this._callback.push(callback);
+    return () =>
+      (this._callback = this._callback.filter(cb => cb !== callback));
   }
 }
+
+const initialState = { count: 0 };
+
+const store = new Store(updateState, initialState);
+
 const incrementAction = { type: "INCREMENT", amount: 5 };
 const decrementAction = { type: "DECREMENT", amount: 3 };
 
-state = updateState(state, incrementAction);
-console.log(state);
+const unsubscribe = store.subscribe(() =>
+  console.log("Новый state " + store.state)
+);
+
+store.subscribe(() => console.log("Новый state = " + store.state));
+
+store.updete(incrementAction);
+unsubscribe();
+store.updete(decrementAction);
+store.updete({});
